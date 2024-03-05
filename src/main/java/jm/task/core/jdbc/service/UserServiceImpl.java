11c5e0f4b6b2514b6de1aca7b175595bd2a018f9
Anton Darenskikh <darenskikh.spb@yandex.ru>
2024-03-05 11:15:48 +0300

    final version

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoJDBCImpl();
     //private final UserDao dao = new UserDaoHibernateImpl();
     public UserServiceImpl() {
     }
